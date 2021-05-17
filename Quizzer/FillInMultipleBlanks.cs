c9fe045a9f21911f757b778152a4e2514038406c
Rebekah Gall <fyrhino@gmail.com>
2021-05-17 12:26:12 -0500

    changed fillinmultiple blank check logic

--- Quizzer/FillInMultipleBlanks.cs
+++ Quizzer/FillInMultipleBlanks.cs
@@ -17,21 +17,30 @@ namespace Quizzer
             string guess1 = placeholderguess1.ToUpper();
             string placeholderguess2 = Console.ReadLine();
             string guess2 = placeholderguess2.ToUpper();
-            if (guess1 == BlankAnswer1 & guess2 == BlankAnswer2)
+
+            bool blank1Right = (guess1 == BlankAnswer1) || (guess1 == BlankAnswer2);
+            bool blank2Right = (guess2 == BlankAnswer1) || (guess2 == BlankAnswer2);
+
+            bool bothRight = (blank1Right) && (blank2Right);
+
+            bool oneRight = (blank1Right || blank2Right) &&     // One or the other is correct,
+                !(blank1Right && blank2Right);                  // But not both at the same time.
+
+            if (bothRight)//guess1 == BlankAnswer1 && guess2 == BlankAnswer2)
             {
                 Console.WriteLine("You got this one right I suppose");
                 return true;
             }
-            else if (guess1 == BlankAnswer1 & guess2 != BlankAnswer2)
+            else if (oneRight)//guess1 == BlankAnswer1 && guess2 != BlankAnswer2)
             {
                 Console.WriteLine("You missed one. So suck it loser");
                 return false;
             }
-            else if (guess1 != BlankAnswer1 & guess2 == BlankAnswer2)
-            {
-                Console.WriteLine("Missed one! Jerkbag");
-                return false;
-            }
+            //else if (guess1 != BlankAnswer1 && guess2 == BlankAnswer2)
+            //{
+            //    Console.WriteLine("Missed one! Jerkbag");
+            //    return false;
+            //}
             else
             {
                 Console.WriteLine("You missed them both you dolt");
